3f37780636d2d9f32eeca75b424febf565a76603
Timur Usmanov <t.usmanov@innopolis.university>
2025-07-11 12:10:37 +0300

    Refactor sql/teachers.py

--- backend/sql/teachers.py
+++ backend/sql/teachers.py
@@ -1,30 +1,37 @@
-def select_course_teachers(db_cursor, course_id):
-    db_cursor.execute(
-        """
-        SELECT t.email, u.publicname
-        FROM teaches t
-        JOIN users u ON t.email = u.email
-        WHERE t.courseid = %s
-        """,
-        (course_id,),
-    )
-    return db_cursor.fetchall()
+from sql.dto import UserEmailNameDTO
 
 
-def insert_teacher(db_cursor, new_teacher_email, course_id):
-    db_cursor.execute(
-        "INSERT INTO teaches (email, courseid) VALUES (%s, %s)",
-        (new_teacher_email, course_id),
-    )
+def select_course_teachers(conn, course_id: str) -> list[UserEmailNameDTO]:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute(
+            """
+            SELECT t.email, u.publicname
+            FROM teaches t
+            JOIN users u ON t.email = u.email
+            WHERE t.courseid = %s
+            """,
+            (course_id,),
+        )
+        return [UserEmailNameDTO(*attrs) for attrs in db_cursor.fetchall()]
 
 
-def count_teachers(db_cursor, course_id):
-    db_cursor.execute("SELECT COUNT(*) FROM teaches WHERE courseid = %s", (course_id,))
-    return db_cursor.fetchone()[0]
+def insert_teacher(conn, new_teacher_email: str, course_id: str) -> None:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute(
+            "INSERT INTO teaches (email, courseid) VALUES (%s, %s)",
+            (new_teacher_email, course_id),
+        )
 
 
-def delete_teacher(db_cursor, course_id, removing_teacher_email):
-    db_cursor.execute(
-        "DELETE FROM teaches WHERE courseid = %s AND email = %s",
-        (course_id, removing_teacher_email),
-    )
+def count_teachers(conn, course_id: str) -> int:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute("SELECT COUNT(*) FROM teaches WHERE courseid = %s", (course_id,))
+        return db_cursor.fetchone()[0]
+
+
+def delete_teacher(conn, course_id: str, removing_teacher_email: str) -> None:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute(
+            "DELETE FROM teaches WHERE courseid = %s AND email = %s",
+            (course_id, removing_teacher_email),
+        )
