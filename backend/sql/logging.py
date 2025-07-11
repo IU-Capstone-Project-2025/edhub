a01d4d0bfd1c01cfb74ae08ac429a46067dd4106
Timur Usmanov <t.usmanov@innopolis.university>
2025-07-11 11:29:02 +0300

    Refactor sql/logs.py

--- backend/sql/logging.py
+++ backend/sql/logging.py
@@ -2,11 +2,13 @@ import random
 LOG_CLEANUP_PROBABILITY = 0.01
 
 
-def insert_log(db_cursor, tag, msg):
-    db_cursor.execute("INSERT INTO logs (t, tag, msg) VALUES (now(), %s, %s)", (tag, msg))
+def insert_log(conn, tag: str, msg: str) -> None:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute("INSERT INTO logs (t, tag, msg) VALUES (now(), %s, %s)", (tag, msg))
     if random.random() < LOG_CLEANUP_PROBABILITY:
-        delete_old_logs(db_cursor)
+        delete_old_logs(conn)
 
 
-def delete_old_logs(db_cursor):
-    db_cursor.execute("DELETE FROM logs WHERE t < NOW() - INTERVAl '7 days'")
+def delete_old_logs(conn) -> None:
+    with conn.cursor() as db_cursor:
+        db_cursor.execute("DELETE FROM logs WHERE t < NOW() - INTERVAl '7 days'")
