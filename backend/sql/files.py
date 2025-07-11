eda4b954abce7cda897eae8772c9409c739cf802
Timur Usmanov <t.usmanov@innopolis.university>
2025-07-11 11:27:35 +0300

    Refactor sql/files.py

--- backend/sql/files.py
+++ backend/sql/files.py
@@ -1,3 +1,4 @@
-def download_attachment(storage_db_cursor, file_id):
-    storage_db_cursor.execute("SELECT content FROM files WHERE id = %s", (file_id, ))
-    return storage_db_cursor.fetchone()
+def download_attachment(storage_conn, file_id: str) -> bytes:
+    with storage_conn.cursor() as storage_db_cursor:
+        storage_db_cursor.execute("SELECT content FROM files WHERE id = %s", (file_id,))
+        return storage_db_cursor.fetchone()[0].tobytes()
